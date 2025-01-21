e5eea18008729aa298a3dddad5f0d1f414fe3383
hiroto-onozawa <s2223026@cco.kanagawa-it.ac.jp>
2025-01-21 17:44:39 +0900

    Light設置(再)

--- Assets/Script/Light2.cs
+++ Assets/Script/Light2.cs
@@ -15,7 +15,10 @@ public class Light2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int i = 0; i < firstfloorlight.Length;i++)
+        {
+            firstfloorlight[i].SetActive(false);
+        }
     }
 
     // Update is called once per frame
