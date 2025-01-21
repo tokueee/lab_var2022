d6b7aa9624810bf578bd398edbdbf3b71dcea1b4
hiroto-onozawa <s2223026@cco.kanagawa-it.ac.jp>
2025-01-21 16:46:25 +0900

    Light設置

---
+++ Assets/Script/Light2.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Light2 : MonoBehaviour
+{
+    //Directional Light Rotation X = -13
+    //S_,“ñŠKƒ‰ƒCƒg@T_.OŠKƒ‰ƒCƒg
+    [SerializeField] private GameObject[] firstfloorlight_h;//ˆêŠK˜L‰º
+    [SerializeField] private GameObject[] firstfloorlight;//ˆêŠKº“à    
+    [SerializeField] private GameObject[] secondfloorlight_h;//“ñŠK˜L‰º
+    [SerializeField] private GameObject[] secondfloorlight;//“ñŠKº“à
+    [SerializeField] private GameObject[] thirdfloorlight_h;//OŠK˜L‰º
+    [SerializeField] private GameObject[] thirdfloorlight;//OŠKº“à
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
