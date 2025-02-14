9f9fff8ecee311bf7d4078d457b1821f8885478b
tokueee <shibashibamasaki@i.softbank.jp>
2025-02-14 12:43:58 +0900

    ボーイキャラクターをAssetStoreからインポートした

--- Assets/Script/Boy.cs
+++ Assets/Script/Boy.cs
@@ -11,39 +11,33 @@ public class Boy : MonoBehaviour
 
     private NavMeshAgent navMeshAgent;
 
-    [SerializeField] private float Delaytime;
+    private Animator animator; // Animatorの追加
+    private static readonly int SpeedParameter = Animator.StringToHash("Speed");
 
     public bool isPlayer = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        animator = this.GetComponent<Animator>();
         navMeshAgent = GetComponent<NavMeshAgent>();
-        StartCoroutine(Delay());
     }
 
-    // Update is called once per fram
-    public IEnumerator Delay()
-    {
-        while (true)
-        {
-            if (isPlayer == true)
-            {
-                Debug.Log("スタート");
-                yield return new WaitForSeconds(Delaytime);
-                Debug.Log("終わり");
-                navMeshAgent.destination = Player.transform.position;
-            }
-            {
-                yield return null; // isPlayerがfalseの間は待機
-            }
-        }
-    }
-
+  
     private void Update()
     {
         With_Friend.SetActive(isPlayer);
         Only_My.SetActive(!isPlayer);
+        OnAnimatorMove();
+        if (isPlayer == true)
+        {
+            navMeshAgent.destination = Player.transform.position;
+        }
     }
 
+    private void OnAnimatorMove()
+    {
+        float speed = navMeshAgent.velocity.magnitude;
+        animator.SetFloat(SpeedParameter, speed);
+    }
 }
